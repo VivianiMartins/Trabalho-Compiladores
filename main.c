fedf84a84d87b340e32311e20f5217afc2f03cc7
Viviani Martins dos Santos <71647406+VivianiMartins@users.noreply.github.com>
2025-07-05 17:36:38 -0300

    Remoção de arquivos a mais
    Retirada de arquivos para funções e alteração para modularização no arquivo principal

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funcoes.h"
+#include <ctype.h>
+
+/*Declaração de funções*/
+void extract_plain_text(FILE *rtf_file, char *output_buffer, int buffer_size);
 
 int main()
 {
@@ -27,4 +30,43 @@ int main()
 
     printf("Hello world!\n");
     return 0;
+}
+
+
+/*função para extrair texto*/
+void extract_plain_text(FILE *rtf_file, char *output_buffer, int buffer_size) {
+    int ch;
+    int in_control_word = 0;
+    int brace_level = 0;
+    int buffer_index = 0;
+
+    while ((ch = fgetc(rtf_file)) != EOF) {
+        if (ch == '\\') {
+            in_control_word = 1;
+        } else if (in_control_word) {
+            if (!isalpha(ch)) {
+                in_control_word = 0;
+                if (ch == '{') {
+                    brace_level++;
+                } else if (ch == '}') {
+                    brace_level--;
+                } else {
+                    if (ch != '{' && ch != '}') {
+                        if (buffer_index < buffer_size - 1) {
+                            output_buffer[buffer_index++] = ch;
+                        }
+                    }
+                }
+            }
+        } else if (ch == '{') {
+            brace_level++;
+        } else if (ch == '}') {
+            brace_level--;
+        } else {
+            if (buffer_index < buffer_size - 1) {
+                output_buffer[buffer_index++] = ch;
+            }
+        }
+    }
+    output_buffer[buffer_index] = '\0';
 }
