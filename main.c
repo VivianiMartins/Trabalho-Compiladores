d1b1ac0e95eb4f3974f6f09068f5d5e041b26dbd
Viviani Martins dos Santos <71647406+VivianiMartins@users.noreply.github.com>
2025-07-05 15:33:21 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    #carregar documento de entrada
+
+    #mémoria
+
+    #verfificar cada linha
+
+    printf("Hello world!\n");
+    return 0;
+}
