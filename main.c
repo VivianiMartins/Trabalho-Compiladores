40f517eeda75add246bc7c594283b2d0ae8a22f2
Viviani Martins dos Santos <71647406+VivianiMartins@users.noreply.github.com>
2025-07-05 17:08:00 -0300

    Modularização
    Teste unindo com o que o Luiz fez. Modularizando código.

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "funcoes.h"
 
 int main()
 {
-    #carregar documento de entrada
+    /*carregar documento de entrada*/
+    FILE *rtf_file = fopen("exemplo.txt", "r");
 
-    #mémoria
+    if (!rtf_file) {
+        perror("Error opening RTF file");
+        return 1;
+    }
 
-    #verfificar cada linha
+    char plain_text_buffer[1024];
+    extract_plain_text(rtf_file, plain_text_buffer, sizeof(plain_text_buffer));
+
+    printf("Extracted Plain Text:\n%s\n", plain_text_buffer);
+
+    fclose(rtf_file);
+
+
+
+    /*mémoria*/
+
+    /*verfificar cada linha*/
 
     printf("Hello world!\n");
     return 0;
